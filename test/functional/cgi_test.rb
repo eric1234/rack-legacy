0cc416f2a638fc5995adb30b8bcf71e1a118a3ec
Christian Rishøj <christian@rishoj.net>
2011-09-07 14:20:27 +0200

    Mechanize 2.0

--- test/functional/cgi_test.rb
+++ test/functional/cgi_test.rb
@@ -8,7 +8,7 @@ require 'rack/legacy/cgi'
 class CgiTest < Test::Unit::TestCase
 
   def test_success
-    response = WWW::Mechanize.new.get 'http://localhost:4000/success.cgi'
+    response = Mechanize.new.get 'http://localhost:4000/success.cgi'
     assert_equal 'Success', response.body
     assert_equal '200', response.code
     assert_equal 'text/html', response.header['content-type']
@@ -16,8 +16,8 @@ class CgiTest < Test::Unit::TestCase
 
   def test_error
     begin
-      WWW::Mechanize.new.get 'http://localhost:4000/error.cgi'
-    rescue WWW::Mechanize::ResponseCodeError
+      Mechanize.new.get 'http://localhost:4000/error.cgi'
+    rescue Mechanize::ResponseCodeError
       assert_match /Internal Server Error/, $!.page.body
       assert_equal '500', $!.page.code
       assert_equal 'text/html', $!.page.header['content-type']
@@ -26,8 +26,8 @@ class CgiTest < Test::Unit::TestCase
 
   def test_syntax_error
     begin
-      WWW::Mechanize.new.get 'http://localhost:4000/syntax_error.cgi'
-    rescue WWW::Mechanize::ResponseCodeError
+      Mechanize.new.get 'http://localhost:4000/syntax_error.cgi'
+    rescue Mechanize::ResponseCodeError
       assert_match /Internal Server Error/, $!.page.body
       assert_equal '500', $!.page.code
       assert_equal 'text/html', $!.page.header['content-type']
@@ -35,34 +35,34 @@ class CgiTest < Test::Unit::TestCase
   end
 
   def test_not_found
-    response = WWW::Mechanize.new.get 'http://localhost:4000/not_found.cgi'
+    response = Mechanize.new.get 'http://localhost:4000/not_found.cgi'
     assert_match 'Endpoint', response.body
     assert_equal '200', response.code
     assert_equal 'text/html', response.header['content-type']
   end
 
   def test_with_status
-    assert_raises WWW::Mechanize::ResponseCodeError do
-      WWW::Mechanize.new.get 'http://localhost:4000/404.cgi'
+    assert_raises Mechanize::ResponseCodeError do
+      Mechanize.new.get 'http://localhost:4000/404.cgi'
     end
   end
 
   def test_multiple_headers
-    response = WWW::Mechanize.new.get 'http://localhost:4000/dup_headers.cgi'
+    response = Mechanize.new.get 'http://localhost:4000/dup_headers.cgi'
     assert_equal '200', response.code
     assert_equal 'cookie1, cookie2', response.header['set-cookie']
     assert_equal 'text/html', response.header['content-type']
   end
 
   def test_querystring
-    response = WWW::Mechanize.new.get 'http://localhost:4000/param.cgi', :q => 'query'
+    response = Mechanize.new.get 'http://localhost:4000/param.cgi', :q => 'query'
     assert_match 'query', response.body
     assert_equal '200', response.code
     assert_equal 'text/html', response.header['content-type']
   end
 
   def test_post
-    response = WWW::Mechanize.new.post 'http://localhost:4000/param.cgi', :q => 'post'
+    response = Mechanize.new.post 'http://localhost:4000/param.cgi', :q => 'post'
     assert_match 'post', response.body
     assert_equal '200', response.code
     assert_equal 'text/html', response.header['content-type']
@@ -72,7 +72,7 @@ class CgiTest < Test::Unit::TestCase
     assert_nothing_raised do
       # 5 seconds should be enough to know it didn't lock up
       timeout 5 do
-        response = WWW::Mechanize.new.get 'http://localhost:4000/flush.cgi'
+        response = Mechanize.new.get 'http://localhost:4000/flush.cgi'
         assert_equal '200', response.code
         assert_equal 'text/html', response.header['content-type']
       end
@@ -81,7 +81,7 @@ class CgiTest < Test::Unit::TestCase
 
   def test_file_upload
     to_upload = File.join File.dirname(__FILE__), '../fixtures/uploaded_file.txt'
-    response = WWW::Mechanize.new.post(
+    response = Mechanize.new.post(
       'http://localhost:4000/upload.cgi',
       {:test => File.new(to_upload)}
     )
