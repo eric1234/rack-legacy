72ab2f18bde792027a61009be381f62fb8222a1e
Christian Rishøj <christian@rishoj.net>
2011-09-07 14:19:47 +0200

    The WEBrick Rack handler seems to expect the body parts as an array

--- test/test_server.rb
+++ test/test_server.rb
@@ -15,6 +15,6 @@ app = Rack::Builder.app do
   use Rack::Legacy::Php, File.join(File.dirname(__FILE__), 'fixtures')
   use Rack::Legacy::Cgi, File.join(File.dirname(__FILE__), 'fixtures')
 
-  run lambda {|env| [200, {'Content-Type' => 'text/html'}, 'Endpoint']}
+  run lambda { |env| [200, {'Content-Type' => 'text/html'}, ['Endpoint']] }
 end
 Rack::Handler::WEBrick.run app, :Port => 4000
